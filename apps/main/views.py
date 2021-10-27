710be1df45b89aa1331d4d8b80f695dd8308a1cc
krylovilya1 <krylovilya1@gmail.com>
2021-10-27 16:25:38 +0700

    Add django-constance, add a plug with technical work

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,3 +1,4 @@
+from constance import config
 from django.views.generic.base import TemplateView
 
 
@@ -5,3 +6,4 @@ class IndexView(TemplateView):
     """Главная страница"""
 
     template_name = "main/index.html"
+    extra_context = {'turn_on_block': config.MAINTENANCE_MODE}
