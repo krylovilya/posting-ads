59deaceae6e33ac0c012c4d056528ecf5e1e60da
krylovilya1 <krylovilya1@gmail.com>
2021-11-21 20:26:03 +0700

    Add Docker, Redis, and Celery

---
+++ server/config/celery.py
@@ -0,0 +1,13 @@
+import os
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+app = Celery("celery_django")
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+
+@app.task
+def add(x, y):
+    return x / y
