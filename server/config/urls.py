d1a3f5d1682c6f8b64a289630a2949feb20e2a5d
krylovilya1 <krylovilya1@gmail.com>
2021-12-19 04:52:52 +0800

    Add sitemap

--- server/config/urls.py
+++ server/config/urls.py
@@ -2,11 +2,21 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.sitemaps.views import sitemap
+from config.sitemaps import StaticViewSitemap
+
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.main.urls')),
     path('accounts/', include('allauth.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
+
 ]
 
 if settings.DEBUG:
