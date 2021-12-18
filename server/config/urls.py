0d8b6503d8164324989482afe3f39dce5fdeb3b0
krylovilya1 <krylovilya1@gmail.com>
2021-12-19 05:09:16 +0800

    Add robots.txt

--- server/config/urls.py
+++ server/config/urls.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
 from django.contrib.sitemaps.views import sitemap
-from config.sitemaps import StaticViewSitemap
+from django.urls import include, path
 
+from apps.main.views import RobotsTxtView
+from config.sitemaps import StaticViewSitemap
 
 sitemaps = {
     'static': StaticViewSitemap,
@@ -15,7 +16,8 @@ urlpatterns = [
     path('', include('apps.main.urls')),
     path('accounts/', include('allauth.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap')
+         name='django.contrib.sitemaps.views.sitemap'),
+    path("robots.txt", RobotsTxtView.as_view())
 
 ]
 
